14787368e3a85b476eaaf9c76229655234178c57
Sergio Romero <sromerojr1015@gmail.com>
2019-09-18 23:50:24 -0400

    Additional review

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,8 +10,7 @@ $(document).ready(function() {
     var num3= Math.floor(Math.random()*11+1)
     var num4= Math.floor(Math.random()*11+1)
     // Setting up random numbers for each jewel
-    // Random number has to be between 1 - 12
-    // 
+    //
     var userTotal= 0; 
     var wins= 0;
     var losses = 0;
@@ -20,12 +19,12 @@ $(document).ready(function() {
   $('#numberLosses').text(losses);
   //resets the game
   function reset(){
-        Random=Math.floor(Math.random()*101+19);
+        Random=Math.floor(Math.random() * 120 + 19);
         $('#randomNumber').text(Random);
-        num1= Math.floor(Math.random()*11+1);
-        num2= Math.floor(Math.random()*11+1);
-        num3= Math.floor(Math.random()*11+1);
-        num4= Math.floor(Math.random()*11+1);
+        num1= Math.floor(Math.random() * 11 + 1);
+        num2= Math.floor(Math.random() * 11 + 1);
+        num3= Math.floor(Math.random() * 11 + 1);
+        num4= Math.floor(Math.random() * 11 + 1);
         userTotal= 0;
         $('#finalTotal').text(userTotal);
         } 
