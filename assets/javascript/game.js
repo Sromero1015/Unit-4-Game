f78080279a74bfd77bf2533f48445fd28dc8fb5a
Sergio Romero <sromerojr1015@gmail.com>
2019-09-18 23:56:31 -0400

    Additional stylizing to be considered

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,10 +5,10 @@ $(document).ready(function() {
     $('#randomNumber').text(Random);
     // Appending random number to the randomNumber id in the html doc
     //
-    var num1= Math.floor(Math.random()*11+1)
-    var num2= Math.floor(Math.random()*11+1)
-    var num3= Math.floor(Math.random()*11+1)
-    var num4= Math.floor(Math.random()*11+1)
+    var num1= Math.floor(Math.random() * 11 + 1)
+    var num2= Math.floor(Math.random() * 11 + 1)
+    var num3= Math.floor(Math.random() * 11 + 1)
+    var num4= Math.floor(Math.random() * 11 + 1)
     // Setting up random numbers for each jewel
     //
     var userTotal= 0; 
